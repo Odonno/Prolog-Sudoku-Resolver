269aa68e13bcc7520200331f10b0284a829620db
Odonno <bottiau.david@laposte.net>
2014-04-18 18:22:04 +0200

    Simplify conversion of Sudoku (using regex) / Implement ConvertSudokuToPrologTab method

--- PrologSudoku/PrologSudoku/PrologSudoku.Services/Infrastructure/SudokuHelper.cs
+++ PrologSudoku/PrologSudoku/PrologSudoku.Services/Infrastructure/SudokuHelper.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿using System.Text;
 using System.Text.RegularExpressions;
 using PrologSudoku.Model.Model.Abstract;
 
@@ -8,16 +8,31 @@ namespace PrologSudoku.Services.Infrastructure
     {
         public static string ConvertSudokuToPrologTab(this ISudoku sudoku)
         {
-            throw new NotImplementedException();
+            var sbuilder = new StringBuilder();
+
+            // Start the array
+            sbuilder.Append("[");
+
+            // Add every values
+            foreach (ISquare t in sudoku.Squares)
+                sbuilder.Append(string.Format("{0},", t.Value));
+
+            // Delete the last character (',')
+            sbuilder.Remove(sbuilder.Length - 1, 1);
+
+            // End the array
+            sbuilder.Append("]");
+
+            return sbuilder.ToString();
         }
 
         public static void ConvertPrologTabToSudoku(this ISudoku sudoku, string prologTab)
         {
-            var regex = new Regex("[\\[\\]]");
-            string[] values = regex.Replace(prologTab, "").Split(',');
+            var regex = new Regex("[\\[,\\]]");
+            string values = regex.Replace(prologTab, "");
 
             for (int i = 0; i < values.Length; i++)
-                sudoku.Squares[i].Value = short.Parse(values[i]);
+                sudoku.Squares[i].Value = short.Parse(values.Substring(i, 1));
         }
     }
 }
--- PrologSudoku/PrologSudoku/PrologSudoku.Test/SudokuUnitTest.cs
+++ PrologSudoku/PrologSudoku/PrologSudoku.Test/SudokuUnitTest.cs
@@ -35,5 +35,30 @@ namespace PrologSudoku.Test
             Assert.AreEqual(sudoku.Squares[7].Value, 8);
             Assert.AreEqual(sudoku.Squares[8].Value, 9);
         }
+
+        [TestMethod]
+        public void Can_Convert_Sudoku_To_Prolog_Tab()
+        {
+            // arrange
+            var sudoku = new Sudoku();
+
+            for (short i = 0; i < 9; i++)
+                for (short j = 0; j < 9; j++)
+                    sudoku.Squares[i * 9 + j].Value = (short)(j + 1);
+
+            // act
+            string prologTab = sudoku.ConvertSudokuToPrologTab();
+
+            // assert
+            Assert.AreEqual(prologTab, "[1,2,3,4,5,6,7,8,9," +
+                                       "1,2,3,4,5,6,7,8,9," +
+                                       "1,2,3,4,5,6,7,8,9," +
+                                       "1,2,3,4,5,6,7,8,9," +
+                                       "1,2,3,4,5,6,7,8,9," +
+                                       "1,2,3,4,5,6,7,8,9," +
+                                       "1,2,3,4,5,6,7,8,9," +
+                                       "1,2,3,4,5,6,7,8,9," +
+                                       "1,2,3,4,5,6,7,8,9]");
+        }
     }
 }
