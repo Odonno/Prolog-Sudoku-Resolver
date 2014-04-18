6100f00f445e32654ee8d3df4200c882ef2c878e
matthieu-2807 <m.lefevre.perso@gmail.com>
2014-04-18 08:38:06 +0200

    code
    ajout du code du prof

--- PrologSudoku/PrologSudoku/PrologSudoku.UI/MainWindow.xaml.cs
+++ PrologSudoku/PrologSudoku/PrologSudoku.UI/MainWindow.xaml.cs
@@ -11,6 +11,10 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.IO;
+using System.Diagnostics;
+using System.ComponentModel;
+using System.Threading;
 
 namespace PrologSudoku.UI
 {
@@ -23,5 +27,38 @@ namespace PrologSudoku.UI
         {
             InitializeComponent();
         }
+
+
+        public void Resolution()
+        {
+            //Création du processus Prolog
+            Process P = new Process();
+            P.StartInfo.FileName = "C:/Program Files/swipl/bin/swipl.exe";
+            P.StartInfo.UseShellExecute = false;
+            P.StartInfo.RedirectStandardInput = true;
+            P.StartInfo.RedirectStandardOutput = true;
+            P.Start();
+
+            //Définition Entrée/Sortie du processus Prolog
+            StreamWriter prologInput = P.StandardInput;
+            StreamReader prologOutput = P.StandardOutput;
+
+            //Lecture du fichier .pl
+            prologInput.WriteLine("consult(\'C:/Users/Matthieu/Documents/Prolog/sudoku.pl\').");
+
+            //Envoit d'un prédicat
+            prologInput.WriteLine("sudokuExemple(X).");
+            Thread.Sleep(200);
+
+            //On ferme le processus pour pouvoir lire son contenu
+            P.Kill();
+            String temp = prologOutput.ReadToEnd();
+
+            //Ecriture dans un fichier
+            System.IO.File.AppendAllText("C:/Users/Matthieu/Desktop/sortie_Sudoku.txt", temp);
+            //Console.ReadLine();
+        }
     }
 }
+
+    
